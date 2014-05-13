aea22189b5022e9cdf79c52e1a32f277d9af7589
Acube Innovation <pramodgmenon@acube.co>
2014-05-13 17:22:08 +0530

    styles modified

--- twitter/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-// set correct path!
-require_once('getTwitterFollowers.php');
-// change screen name to yours
-echo getTwitterFollowers('hayatfmuae');
-echo "<br>";
-echo getTwitterFollowers('arunwrc');
-
-?>
